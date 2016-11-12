9c99ee722756d9384ece00c4a01e8f99de386efa
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2016-11-12 20:32:38 +0100

    Code cleanup

--- src/main/java/com/asodc/tinkerforge/PlantAutomation.java
+++ src/main/java/com/asodc/tinkerforge/PlantAutomation.java
@@ -13,10 +13,10 @@ public class PlantAutomation {
 
     private static final String HOST = "PlantAutomation";
     private static final int PORT = 4223;
-    private static final String TEMPERATURE_SENSOR_UID = "zky";
+    private static final String AMBIENT_LIGHT_SENSOR_UID = "yC3";
     private static final String HUMIDITY_SENSOR_UID = "xwj";
     private static final String MOISTURE_SENSOR_UID = "zU3";
-    private static final String AMBIENT_LIGHT_SENSOR_UID = "yC3";
+    private static final String TEMPERATURE_SENSOR_UID = "zky";
 
     public static void main(String[] args) {
         attachTemperature();
@@ -31,14 +31,14 @@ public class PlantAutomation {
         AmbientLightSensor ambientLightSensor = new AmbientLightSensor(AMBIENT_LIGHT_SENSOR_UID, ambientLightConnection);
     }
 
-    private static void attachMoisture() {
-        Connection moistureConnection = new Connection(HOST, PORT);
-        MoistureSensor moistureSensor = new MoistureSensor(MOISTURE_SENSOR_UID, moistureConnection);
-    }
-
     private static void attachHumidity() {
         Connection humidityConnection = new Connection(HOST, PORT);
         HumiditySensor humiditySensor = new HumiditySensor(HUMIDITY_SENSOR_UID, humidityConnection);
+    }
+
+    private static void attachMoisture() {
+        Connection moistureConnection = new Connection(HOST, PORT);
+        MoistureSensor moistureSensor = new MoistureSensor(MOISTURE_SENSOR_UID, moistureConnection);
     }
 
     private static void attachTemperature() {
